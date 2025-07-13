97ba31ce5c23f7b3971331764927200945427ea2
edson2001 <edson.bandola@afrizona.co.ao>
2025-07-13 12:42:53 +0100

    feat: add ticket

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,34 @@
 // middleware.ts
 import { NextRequest, NextResponse } from "next/server";
+import { useUserStore } from "./stores/userStore";
+ 
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("token")?.value;
+  console.log(token, "************");
+  const isProtectedRoute = request.nextUrl.pathname.startsWith("/painel");
+  const isAdminRoute = request.nextUrl.pathname.startsWith("/painel/users");
+
+  if (isProtectedRoute && !token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (isAdminRoute) {
+    // Simulando a obtenção do usuário do userStore (você pode adaptar para buscar do backend)
+    const user = useUserStore.getState().user;
+    if (user?.role !== "ADMIN") {
+      return NextResponse.redirect(new URL("/painel", request.url));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/painel/:path*"],
+};
+/* // middleware.ts
+import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
@@ -16,3 +45,4 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: ["/painel/:path*"],  
 };
+ */
