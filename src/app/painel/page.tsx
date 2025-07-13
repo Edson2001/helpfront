17c4ff3515d3ffc1e73dd8157007ce762ea0b3d5
edson2001 <edson.bandola@afrizona.co.ao>
2025-07-13 14:41:16 +0100

    feat: add clear

--- src/app/painel/page.tsx
+++ src/app/painel/page.tsx
@@ -2,13 +2,7 @@
 
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
-import {
-  AverageTicketsCreated,
-  Conversions,
-  CustomerSatisfication,
-  Metrics,
-  TicketByChannels,
-} from "@/components/chart-blocks";
+import { AverageTicketsCreated, Metrics } from "@/components/chart-blocks";
 import Container from "@/components/container";
 import { useUserStore } from "@/stores/userStore";
 import { useList } from "./hooks/useList";
@@ -18,7 +12,7 @@ export default function Home() {
 
   const pathName = usePathname();
 
-  const { user, loading, error, fetchUser } = useUserStore();
+  const { user, fetchUser } = useUserStore();
 
   useEffect(() => {
     if (!user) {
@@ -33,18 +27,7 @@ export default function Home() {
         <Container className="w-full py-4 laptop:col-span-3">
           <AverageTicketsCreated data={data ?? []} />
         </Container>
-        {/* <Container className="py-4 laptop:col-span-1">
-          <Conversions />
-        </Container> */}
       </div>
-      {/* <div className="grid grid-cols-1 divide-y border-b border-border laptop:grid-cols-2 laptop:divide-x laptop:divide-y-0 laptop:divide-border">
-        <Container className="py-4 laptop:col-span-1">
-          <TicketByChannels />
-        </Container>
-        <Container className="py-4 laptop:col-span-1">
-          <CustomerSatisfication />
-        </Container>
-      </div> */}
     </div>
   );
 }
