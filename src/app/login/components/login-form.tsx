d311c0bd8b54508f828f6fa7e59bec718dde486b
edson2001 <edson.bandola@afrizona.co.ao>
2025-07-14 06:19:08 +0100

    feat: add compact

--- src/app/login/components/login-form.tsx
+++ src/app/login/components/login-form.tsx
@@ -1,11 +1,8 @@
-
-
-
-
 "use client";
 
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import Turnstile from "react-turnstile";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Alert, AlertDescription } from "@/components/ui/alert";
@@ -37,7 +34,8 @@ export function LoginForm({
 
   const loginMutation = useLogin();
   const [error, setError] = useState<string | null>(null);
-/* const { user, loading, error, fetchUser } = useUserStore();
+  const [token, setToken] = useState<string | null>(null);
+  /* const { user, loading, error, fetchUser } = useUserStore();
 
   useEffect(() => {
     if (!user) {
@@ -45,23 +43,30 @@ export function LoginForm({
     }
   }, [user, fetchUser]); */
   const onSubmit = (data: LoginFormData) => {
-    loginMutation.mutate(data, {
-      onError: (error: any) => {
-        if (error.response?.status === 401) {
-          setError(
-            error.response?.data?.message ??
-              "Credenciais inválidas. Por favor, tente novamente.",
-          );
-        } else {
-          setError(
-            "Ocorreu um erro ao tentar fazer login. Tente novamente mais tarde.",
-          );
-        }
+    if (!token) {
+      setError("Por favor, complete a verificação do Turnstile.");
+      return;
+    }
+    loginMutation.mutate(
+      { ...data },
+      {
+        onError: (error: any) => {
+          if (error.response?.status === 401) {
+            setError(
+              error.response?.data?.message ??
+                "Credenciais inválidas. Por favor, tente novamente.",
+            );
+          } else {
+            setError(
+              "Ocorreu um erro ao tentar fazer login. Tente novamente mais tarde.",
+            );
+          }
+        },
+        onSuccess: () => {
+          setError(null);
+        },
       },
-      onSuccess: () => {
-        setError(null);
-      },
-    });
+    );
   };
 
   return (
@@ -111,6 +116,7 @@ export function LoginForm({
             </span>
           )}
         </div>
+        
         <Button
           disabled={loginMutation.isPending}
           type="submit"
@@ -118,6 +124,12 @@ export function LoginForm({
         >
           {loginMutation.isPending ? "Entrando..." : "Entrar"}
         </Button>
+        <Turnstile
+          sitekey={process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY || ""}
+          onVerify={(token) => setToken(token)}
+          theme="light" // ou "dark"
+          size="normal" // ou "compact"
+        />
         <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
           <span className="relative z-10 bg-background px-2 text-muted-foreground"></span>
         </div>
