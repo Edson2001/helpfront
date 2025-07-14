80d0c1bbce93218687d9cf8d2c03e75869dbc8ce
edson2001 <edson.bandola@afrizona.co.ao>
2025-07-14 06:04:26 +0100

    feat: add build

--- src/app/ticket/page.tsx
+++ src/app/ticket/page.tsx
@@ -1,17 +1,11 @@
-"use client"
-import { useSearchParams } from "next/navigation";
-import React from "react";
-import TicketDetailPage from "../painel/ticket/[id]/page";
-import { useTicketByToken } from "./hooks/useTicketByToken";
+// app/ticket/page.tsx
+import React, { Suspense } from "react";
+import MyTicket from "./MyTicket";
 
 export default function Page() {
-  const searchParams = useSearchParams();
-  const token = searchParams.get("token");
-   
-  const { data: ticket, isLoading, error } = useTicketByToken(token || "");
-
-  if (isLoading) return <div>Carregando...</div>;
-  if (error) return <div>Erro ao carregar o ticket.</div>;
-
-  return <TicketDetailPage ticketId={(ticket as any)?.id || ""} userTicket={ticket} />;
+  return (
+    <Suspense fallback={<div>Carregando página...</div>}>
+      <MyTicket />
+    </Suspense>
+  );
 }
