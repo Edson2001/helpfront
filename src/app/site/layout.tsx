1d7e317c149b1cf4d0f8df14b5d380b473a04062
edson2001 <edson.bandola@afrizona.co.ao>
2025-07-15 14:20:00 +0100

    feat: add website

---
+++ src/app/site/layout.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+
+import { ThemeProvider } from "next-themes";
+import { Inter } from "next/font/google";
+//import "../../style/globals.css";
+import Lines from "./components/Lines";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
+const inter = Inter({ subsets: ["latin"] });
+
+ 
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`dark:bg-black ${inter.className}`}>
+        <ThemeProvider
+          enableSystem={false}
+          attribute="class"
+          defaultTheme="light"
+        >
+          <Lines />
+          <Header />
+          {/* <ToasterContext /> */}
+          {children}
+          <Footer />
+          <ScrollToTop />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
