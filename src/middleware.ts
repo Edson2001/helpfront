8f00e2b24efc7b6b6d43ad2adc5cd9ea6ba5c6d5
edson2001 <edson.bandola@afrizona.co.ao>
2025-07-17 09:07:29 +0100

    feat: remove logs form mid

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,12 @@
-// middleware.ts
-import * as cookie from "cookie";
 import { NextRequest, NextResponse } from "next/server";
-import { useUserStore } from "./stores/userStore";
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
   const userToken = request.cookies.get("user")?.value;
-  console.log("Cookie 'user':", userToken); // Depuração
 
-  // Analisar o cookie 'user' para obter os dados do usuário
   let userData = null;
   try {
     userData = userToken ? JSON.parse(userToken) : null;
-    console.log("Dados do usuário analisados:", userData); // Depuração
   } catch (error) {
     console.error("Erro ao analisar o cookie 'user':", error);
   }
@@ -23,8 +17,6 @@ export function middleware(request: NextRequest) {
   if (isProtectedRoute && !token) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
-
-  console.log("user", userToken);
 
   if (isAdminRoute) {
     if (userData?.role !== "ADMIN") {
@@ -38,21 +30,3 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: ["/painel/:path*"],
 };
-/* // middleware.ts
-import { NextRequest, NextResponse } from "next/server";
-
-export function middleware(request: NextRequest) {
-  const token = request.cookies.get("token")?.value;
-  console.log(token, "************")
-  const isProtectedRoute = request.nextUrl.pathname.startsWith("/painel");
-
-  if (isProtectedRoute && !token) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
-  return NextResponse.next();
-}
-export const config = {
-  matcher: ["/painel/:path*"],  
-};
- */
